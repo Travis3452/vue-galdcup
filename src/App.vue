7dfcf2fd70ed51ba0a71cca4e52c1c65750f8c66
Travis3452 <rnjswlsdn3452@gmail.com>
2026-01-07 19:41:39 +0900

    refactor: 서브창 사용을 위한 noLayout 설정 추가

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <!-- 상단 NavBar -->
-    <NavBar />
+    <NavBar v-if="!$route.meta.noLayout" />
 
     <!-- 메인 콘텐츠 -->
     <main class="flex-1">
@@ -9,13 +9,16 @@
     </main>
 
     <!-- 하단 Footer -->
-    <Footer />
+    <Footer v-if="!$route.meta.noLayout" />
   </div>
 </template>
 
 <script setup>
 import NavBar from './components/NavBar.vue'
 import Footer from './components/Footer.vue'
+import { useRoute } from 'vue-router'
+
+const $route = useRoute()
 </script>
 
 <style scoped>
